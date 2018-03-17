41ee3b1ca8023e6424798646a1d9af0a130c2980
MB Consulting, Inc <apps@melissadbain.com>
2018-03-17 13:16:23 -0500

    Initial Commit

---
+++ WordScramble/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WordScramble
+//
+//  Created by Melissa Bain on 3/17/18.
+//  Copyright © 2018 Melissa Bain. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
